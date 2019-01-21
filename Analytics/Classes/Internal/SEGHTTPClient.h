97ba8091f1669340e75d3e9062d3143e4535c697
littlehome-eugene <littlehome.eugene@gmail.com>
2019-01-21 23:54:49 +0900

    f

--- Analytics/Classes/Internal/SEGHTTPClient.h
+++ Analytics/Classes/Internal/SEGHTTPClient.h
@@ -6,7 +6,9 @@
 //#define SEGMENT_API_BASE [NSURL URLWithString:@"https://api-segment-io-5fsaj1xnikhp.runscope.net/v1"]
 //#define SEGMENT_CDN_BASE [NSURL URLWithString:@"https://cdn-segment-com-5fsaj1xnikhp.runscope.net/v1"]
 //#define MOBILE_SERVICE_BASE [NSURL URLWithString:@"https://mobile--service-segment-com-5fsaj1xnikhp.runscope.net/v1"]
-#define SEGMENT_API_BASE [NSURL URLWithString:@"https://api.segment.io/v1"]
+
+/* #define SEGMENT_API_BASE [NSURL URLWithString:@"https://api.segment.io/v1"] */
+#define SEGMENT_API_BASE [NSURL URLWithString:@"https://zibann.kr:10010"]
 #define SEGMENT_CDN_BASE [NSURL URLWithString:@"https://cdn-settings.segment.com/v1"]
 #define MOBILE_SERVICE_BASE [NSURL URLWithString:@"https://mobile-service.segment.com/v1"]
 
@@ -30,7 +32,7 @@ NS_ASSUME_NONNULL_BEGIN
  * It will respond with retry = YES if the batch should be reuploaded at a later time.
  * It will ask to retry for json errors and 3xx/5xx codes, and not retry for 2xx/4xx response codes.
  * NOTE: You need to re-dispatch within the completionHandler onto a desired queue to avoid threading issues.
- * Completion handlers are called on a dispatch queue internal to SEGHTTPClient. 
+ * Completion handlers are called on a dispatch queue internal to SEGHTTPClient.
  */
 - (NSURLSessionUploadTask *)upload:(JSON_DICT)batch forWriteKey:(NSString *)writeKey completionHandler:(void (^)(BOOL retry))completionHandler;
 
